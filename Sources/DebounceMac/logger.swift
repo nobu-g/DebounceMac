9bacbbb2507cbe07bb116583a9f73ae63836084b
nobu-g <ueda-n127@hotmail.co.jp>
2020-10-24 22:54:02 +0900

    color output

--- Sources/DebounceMac/logger.swift
+++ Sources/DebounceMac/logger.swift
@@ -16,6 +16,7 @@ func setupLogger() -> SwiftyBeaver.Type {
         let file = FileDestination()
         file.logFileURL = URL(fileURLWithPath: "\(NSHomeDirectory())/Library/Caches/debounce_mac.log")  // default: ~/Library/Caches/swiftybeaver.log
         file.format = "$Dyyyy-MM-dd HH:mm:ss.SSS$d $C$L$c $N.$F:$l - $M"  // 2019-08-04 19:42:51.612 INFO main.tapEvents():54 - Initializing an event tap.
+        file.colored = true
         file.minLevel = .info  // default: .verbose
         return file
     }()
