6e583b41e972d60091703ee842a32518daa37f02
nobu-g <ueda-n127@hotmail.co.jp>
2023-02-04 03:03:03 +0900

    update deps

--- Package.swift
+++ Package.swift
@@ -6,9 +6,9 @@ let package = Package(
     name: "DebounceMac",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", from: "1.9.2"),
-         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
-         .package(url: "https://github.com/lukaskubanek/OrderedDictionary.git", from: "3.0.1"),
+         .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", from: "1.9.6"),
+         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.1"),
+         .package(url: "https://github.com/lukaskubanek/OrderedDictionary.git", from: "4.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
