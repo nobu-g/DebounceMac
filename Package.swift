774be42501217d35e1768660ae10179ea7a15e2f
nobu-g <ueda-n127@hotmail.co.jp>
2025-05-17 16:50:32 +0900

    update apple/swift-collections

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
          .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", from: "2.1.1"),
          .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.2"),
-         .package(url: "https://github.com/apple/swift-collections", from: "1.1.1"),
+         .package(url: "https://github.com/apple/swift-collections", from: "1.1.4"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
